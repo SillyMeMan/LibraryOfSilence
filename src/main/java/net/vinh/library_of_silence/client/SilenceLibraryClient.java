6e6c5debb0ae91fac3ddeb86ae96711820114795
BuildTools <unconfigured@null.spigotmc.org>
2025-08-28 15:18:52 +0700

    First commit

---
+++ src/main/java/net/vinh/library_of_silence/client/SilenceLibraryClient.java
@@ -0,0 +1,10 @@
+package net.vinh.library_of_silence.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SilenceLibraryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
